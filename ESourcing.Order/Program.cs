41eab8ed76d3ecc301ef8fad58f56ff0e60ad443
HASAND <HASAND@HASAND-NB>
2021-01-20 20:14:21 +0300

    fix

--- ESourcing.Order/Program.cs
+++ ESourcing.Order/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ordering.Infrastructure.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,9 @@ namespace ESourcing.Order
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            CreateAndSeedDatabase(host);
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +26,25 @@ namespace ESourcing.Order
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void CreateAndSeedDatabase(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+                try
+                {
+                    var aspnetRunContext = services.GetRequiredService<OrderContext>();
+                    OrderContextSeed.SeedAsync(aspnetRunContext, loggerFactory).Wait();
+                }
+                catch (Exception exception)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(exception, "An error occurred seeding the DB.");
+                }
+            }
+        }
     }
 }
