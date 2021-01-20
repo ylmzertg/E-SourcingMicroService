4a1c3956e19cfb2ecdf100a57123303fadb96c5a
HASAND <HASAND@HASAND-NB>
2021-01-20 15:21:37 +0300

    fix

---
+++ Order.Infrastructure/Repository/OrderRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repository.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repository
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersBySellerUserName(string userName)
+        {
+            var orderList = await _dbContext.Orders
+                      .Where(o => o.SellerUserName == userName)
+                      .ToListAsync();
+
+            return orderList;
+        }
+    }
+}
